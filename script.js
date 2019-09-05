8ec0c6ffb0d0f43b7d1e54008a69ed97a2be4df9
Rory Flint <rory@roryflint.co.uk>
2019-09-05 19:07:34 +0100

    sorry again

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const numberHeader = document.querySelector('h1');
 const textHeader = document.querySelector('h2');
 const openingText = document.querySelector('p');
-const menuButton = document.querySelector('.menu-button');
 
-menuButton.addEventListener('click', () => console.log("hello!"))
+const menuButton = document.querySelector('.menu-button');
+const popupBar = document.querySelector('.popup-bar');
+
 
 window.addEventListener('keydown', keyLog);
+menuButton.addEventListener('click', addHeight);
 
 
-function keyLog (event) {
+function keyLog(event) {
 
     event.keyCode === 32
-     ? textHeader.innerText = "Spacebar"
-     : textHeader.innerText = event.key;
+        ? textHeader.innerText = "Spacebar"
+        : textHeader.innerText = event.key;
 
     numberHeader.innerText = event.keyCode;
 
     openingText.style.display = "none";
 }
+
+function addHeight() {
+    popupBar.classList.toggle('height')
+};
 
