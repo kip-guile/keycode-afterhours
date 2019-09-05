82aa55bef91c857f5823290f6290fa5867ee2499
Rory Flint <rory@roryflint.co.uk>
2019-09-05 19:02:56 +0100

    sorry

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const numberHeader = document.querySelector('h1');
 const textHeader = document.querySelector('h2');
+const openingText = document.querySelector('p');
+const menuButton = document.querySelector('.menu-button');
 
+menuButton.addEventListener('click', () => console.log("hello!"))
 
 window.addEventListener('keydown', keyLog);
 
 
 function keyLog (event) {
+
+    event.keyCode === 32
+     ? textHeader.innerText = "Spacebar"
+     : textHeader.innerText = event.key;
+
     numberHeader.innerText = event.keyCode;
+
+    openingText.style.display = "none";
 }
+
